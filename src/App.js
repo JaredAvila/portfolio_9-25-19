0f41074934df0c6f5f632275a717cb6857df000a
Jared Avila <jared.c.avila@gmail.com>
2019-09-25 17:13:19 -0700

    new portfolio 9-25-19

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <ion-icon name="logo-github"></ion-icon>
+      <ion-icon name="logo-linkedin"></ion-icon>
+      <ion-icon name="mail"></ion-icon>
     </div>
   );
 }
